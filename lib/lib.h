10cc9ad31ba8ab245cf0002df1146b84e9bebcc4
Hernan Silva <hernansilva2002@gmail.com>
2024-10-14 15:09:09 -0300

    inclusion de api avr, declaracion de funciones

--- lib/lib.h
+++ lib/lib.h
@@ -1,13 +1,17 @@
-#ifdef _WIN32
-    #define LIB_PATH "C:\\Users\hernan\Documents\TP\lib\"
-#else
-    #define LIB_PATH "../
-#ifdef LIB_H
+//Para hacer cross platform los paths relativos
+ //#ifdef _WIN32
+ //    #define LIB_PATH "C:\\Users\\hernan\\Documents\\TP\lib\\"
+ //#else
+ //    #define LIB_PATH "../
+ //#endif
+#ifndef LIB_H
 #define LIB_H
+#include <stdint.h>
+
 typedef enum {
     voltaje_correcto,
     voltaje_incorrecto
-} estados_t
+} estados_t;
 
 typedef struct {
     uint8_t v; //voltaje actual
@@ -15,7 +19,7 @@ typedef struct {
     uint8_t v_max;
 } voltaje_t;
 
-voltaje_t f_inicio_micro(void);
+void f_inicio_atmega(void);
 estados_t f_voltaje_correcto(voltaje_t);
 estados_t f_voltaje_incorrecto(voltaje_t);
 #endif
---
+++ src/funciones.c
@@ -0,0 +1,10 @@
+#include "../lib/lib.h"
+
+void f_inicio_atmega(void) {
+}
+
+estados_t f_voltaje_correcto(void) {
+}
+
+estados_t f_voltaje_incorrecto(voltaje_t volt_curr) {
+}
